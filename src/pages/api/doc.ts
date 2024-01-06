6d4158c062c9fb31405264a0caaa002b82572891
Nathan Hellinga <nathaniel.hellinga@gmail.com>
2024-01-06 13:30:13 -0500

    include server url in swagger schema

--- src/pages/api/doc.ts
+++ src/pages/api/doc.ts
@@ -1,14 +1,19 @@
-import { withSwagger } from 'next-swagger-doc';
+import { withSwagger } from "next-swagger-doc";
 
 const swaggerHandler = withSwagger({
   definition: {
-    openapi: '3.1.0',
+    openapi: "3.1.0",
     info: {
-      title: 'DND Toolbox API Specs',
-      version: '1.0.0',
+      title: "DND Toolbox API Specs",
+      version: "1.0.0"
     },
+    servers: [
+      {
+        url: process.env.SERVER as string || ""
+      }
+    ]
   },
-  apiFolder: 'src/pages/api',
-  schemaFolders: ['src/models']
+  apiFolder: "src/pages/api",
+  schemaFolders: ["src/models"]
 });
 export default swaggerHandler();
